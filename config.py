159742f33dd79886e74779bc9f23e9ade9db9784
Taro Minowa <higepon@gmail.com>
2017-06-12 21:11:32 +0900

    Tuning data py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Parameters for Data processing
+DATA_PATH = 'data/cornell movie-dialogs corpus'
+CONVO_FILE = 'movie_conversations.txt'
+LINE_FILE = 'movie_lines.txt'
+OUTPUT_FILE = 'output_convo.txt'
+PROCESSED_PATH = 'processed'
+
+CPT_PATH = 'checkpoints'
+PAD_ID = 0
+UNK_ID = 1
+START_ID = 2
+EOS_ID = 3
+
+THRESHOLD = 2
+# Real config parameters to care
+
+BUCKETS = [(8, 10), (12, 14), (16, 19)]
+TESTSET_SIZE = 25000
+
+ENC_VOCAB = 24442
+DEC_VOCAB = 24697
