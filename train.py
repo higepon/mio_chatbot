be9ff86f51c1d68a0eca94945044c3613e3ba5c7
Taro Minowa <higepon@gmail.com>
2017-06-14 08:07:53 +0900

    rnn

--- train.py
+++ train.py
@@ -2,15 +2,33 @@ import tensorflow as tf
 import config
 
 
-single_cell = tf.contrib.rnn.GRUCell(config.HIDDEN_SIZE)
-multi_cell = tf.contrib.rnn.MultiRNNCell([single_cell] * config.NUM_LAYERS)
+def create_seq2seq(encoder_inputs, decoder_inputs, do_decode, cell):
+    return tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
+        encoder_inputs=encoder_inputs,
+        decoder_inputs=decoder_inputs,
+        cell=cell,
+        num_encoder_symbols=config.ENC_VOCAB,
+        num_decoder_symbols=config.DEC_VOCAB,
+        embedding_size=config.HIDDEN_SIZE,
+        output_projection=None, # todo
+        feed_previous=do_decode)
 
-encoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='encoder{}'.format(i))
-                  for i in range(config.BUCKETS[-1][0])]
-decoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='decoder{}'.format(i))
-                  for i in range(config.BUCKETS[-1][1] + 1)]
-decoder_masks = [tf.placeholder(tf.float32, shape=[None], name='mask{}'.format(i))
-                 for i in range(config.BUCKETS[-1][1] + 1)]
 
+def create_model():
+    cells = [tf.contrib.rnn.GRUCell(config.HIDDEN_SIZE) for _ in range(config.NUM_LAYERS)]
+    multi_cell = tf.contrib.rnn.MultiRNNCell(cells=cells)
+    encoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='encoder{}'.format(i))
+                      for i in range(config.BUCKETS[-1][0])]
+    decoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='decoder{}'.format(i))
+                      for i in range(config.BUCKETS[-1][1] + 1)]
+    decoder_masks = [tf.placeholder(tf.float32, shape=[None], name='mask{}'.format(i))
+                     for i in range(config.BUCKETS[-1][1] + 1)]
 
-# tf.contrib.legacy_seq2seq.model_with_buckets()
+    x = create_seq2seq(encoder_inputs=encoder_inputs,
+                       decoder_inputs=decoder_inputs,
+                       do_decode=False,
+                       cell=multi_cell)
+    print(x)
+
+if __name__ == '__main__':
+    create_model()
